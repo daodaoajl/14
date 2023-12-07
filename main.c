e281195b671d9bb3f9a9f2afd7a361d41224bd69
daodaoajl <144930715+daodaoajl@users.noreply.github.com>
2023-12-07 15:27:22 +0900

    2

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void)
+struct Book
 {
-  char* pc = NULL;
-  int i = 0;
+       int number;
+       char title[10];       
+};
+
+int main(void)
+{
+  struct Book*p;
   
-  pc = (char*)malloc(100*sizeof(char));
-  //할당 받음(동적 메모리 할당)
-   
-  if(pc==NULL)
+  p = (struct Book*)malloc( 2*sizeof(struct Book) );
+  
+  if(p == NULL)
   {
-      printf("메모리 할당 오듀\n");
-      exit(1);            
+     printf("메모리 할당 오류\n");
+     return;                       
   }
   
-  for(i=0;i<26;i++)
-  {
-      pc[i] = 'a'+i;                 
-  }
+  p->number = 1;
+  strcpy(p->title, "C Programming");
   
-  pc[i] = 0;
-  printf("%s\n", pc);
+  (p+1)->number = 2;
+  strcpy((p+1)->title, "Electronics");
   
-  free(pc);//동적 메모리 할당 
-  
+  free(p);
+
   system("PAUSE");	
-  return 0;
+  return;
 }
-// result -> [abcdefghijklmnopqrstuvwxyz]
+// result -> compile
